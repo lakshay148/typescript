1504e744f465704868850c1ad211b03a0a593ef3
lakshay <lakshayg@paisabazaar.com>
2020-05-10 02:33:25 +0530

    added google map , @types/googlemaps

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,16 @@
-console.log(`heyy`);
+import { User } from "./User";
+import { Company } from "./Company";
+
+const user = new User();
+const company = new Company();
+
+console.log(`lg user `, user);
+console.log(`lg company `, company);
+
+const map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
